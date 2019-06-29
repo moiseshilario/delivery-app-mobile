17d9a165a74df07518ec9f7123825307b3f9e20a
moiseshilario <moiseshilariu@gmail.com>
2019-06-29 12:55:07 -0300

    Add vector icons

--- android/app/src/main/java/com/modulo4/MainApplication.java
+++ android/app/src/main/java/com/modulo4/MainApplication.java
@@ -3,6 +3,7 @@ package com.modulo4;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new AsyncStoragePackage(),
             new LinearGradientPackage(),
             new RNGestureHandlerPackage()
