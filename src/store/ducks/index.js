0df7538a878ef678231da37552e1afba5285bb82
moiseshilario <moiseshilariu@gmail.com>
2019-07-01 13:45:36 -0300

    Add menu to store

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { toastReducer as toast } from 'react-native-redux-toast';
 
 import auth from './auth';
+import menu from './menu';
 
 export default combineReducers({
   auth,
+  menu,
   toast,
 });
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -4,13 +4,16 @@ import { all, takeLatest } from 'redux-saga/effects';
 // import { navigate } from '~/services/navigation';
 
 import { Types as AuthTypes } from '../ducks/auth';
+import { Types as MenuTypes } from '../ducks/menu';
 
 import { signIn, init, signUp } from './auth';
+import { loadMenu } from './menu';
 
 export default function* rootSaga() {
   yield all([
     init(),
     takeLatest(AuthTypes.SIGNIN_REQUEST, signIn),
     takeLatest(AuthTypes.SIGNUP_REQUEST, signUp),
+    takeLatest(MenuTypes.MENU_REQUEST, loadMenu),
   ]);
 }
