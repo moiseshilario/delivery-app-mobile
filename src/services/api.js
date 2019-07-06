98f6b9e5726dcc00bd4333a3ea540989a1933d7c
moiseshilario <moiseshilariu@gmail.com>
2019-07-06 12:43:15 -0300

    Add token to requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,22 @@
 import axios from 'axios';
+import store from '~/store';
 
 const baseURL = 'http://localhost:3333';
 
 const api = axios.create({
   baseURL,
+});
+
+api.interceptors.request.use((config) => {
+  const { token } = store.getState().auth;
+
+  const headers = { ...config.headers };
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  return { ...config, headers };
 });
 
 export const imagesURL = `${baseURL}/files`;
